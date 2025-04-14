5d2cf8df8bbf2bd4a6d3eaa541c093c4632b335b
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2025-04-14 20:06:42 +0200

    Refactor attribute tests

--- src/test/scala/AttributeSpec.scala
+++ src/test/scala/AttributeSpec.scala
@@ -6,17 +6,14 @@ import matchers.*
 
 class AttributeSpec extends AnyFlatSpec with should.Matchers {
 
-  case class ExampleAttribute(override val parent: Option[Attribute], override val value: String) extends Attribute
-
-  "An Attribute" should "have an optional parent" in {
-    val attribute: Attribute = ExampleAttribute(parent = None, value = "")
-    val parent: Option[Attribute] = attribute.parent
+  "An Attribute" should "have an optional parent" in new Attribute:
+    override val value: String = ""
+    val parent: Option[Attribute] = parent
     assert(parent.isEmpty)
-  }
 
-  it should "have a descriptive value" in {
+  it should "have a descriptive value" in new Attribute:
     val description = "value"
-    val attribute: Attribute = ExampleAttribute(parent = None, value = description)
-    assert(attribute.value == description)
-  }
+    override val value: String = description
+    override val parent: Option[Attribute] = None
+    value shouldBe description
 }
