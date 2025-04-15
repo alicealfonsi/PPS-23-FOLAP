c88f1fb79a42c05bb7a1e0c4526fd74e557f45f3
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-04-15 08:26:23 +0200

    Long measure

--- src/main/scala/Cube.scala
+++ src/main/scala/Cube.scala
@@ -1,7 +1,8 @@
 object Cube:
+  type Number = Int | Long
   trait Attribute:
     val parent: Option[Attribute]
     val value: String
 
-  trait Measure:
-    val value: Int
+  trait Measure[T <: Number]:
+    val value: T
--- src/test/scala/MeasureSpec.scala
+++ src/test/scala/MeasureSpec.scala
@@ -3,10 +3,15 @@ import org.scalatest.*
 import flatspec.*
 import matchers.*
 
-case class ExampleMeasure(override val value: Int) extends Measure
+case class ExampleMeasure[T <: Cube.Number](override val value: T) extends Measure[T]
 
 class MeasureSpec extends AnyFlatSpec with should.Matchers with BeforeAndAfterEach:
   "A Measure" should "accept an integer value" in:
     val value: Int = 42
+    val measure = ExampleMeasure(value)
+    measure.value shouldEqual value
+
+  it should "accept a long value" in :
+    val value: Long = 4242
     val measure = ExampleMeasure(value)
     measure.value shouldEqual value
