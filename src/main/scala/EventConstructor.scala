9b8259d753fa6b9c4b83726201c5b0cf7486fbee
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-04-22 12:41:23 +0200

    Scalafix format

--- src/main/scala/EventConstructor.scala
+++ src/main/scala/EventConstructor.scala
@@ -1,4 +1,6 @@
-import MultidimensionalModel.{Attribute, Measure, Event}
+import MultidimensionalModel.Attribute
+import MultidimensionalModel.Event
+import MultidimensionalModel.Measure
 
 type EventConstructor[A <: Attribute, M <: Measure[_]] =
   (Iterable[A], Iterable[M]) => Event[A, M]
