e9fbecb202fb9abe8810cddce88fd170e564d112
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-04-23 10:14:22 +0200

    fix format

--- src/main/scala/Operators.scala
+++ src/main/scala/Operators.scala
@@ -4,54 +4,28 @@ import MultidimensionalModel._
   * warehouse
   */
 object Operators:
+  
+    def drillAcross[A <: Attribute, M <: Measure[_]](
+        events: Iterable[Event[A, M]],
+        otherEvents: Iterable[Event[A, M]],
+        createEvent: EventConstructor[A, M]
+    ): Iterable[Event[A, M]] = {
+      events.flatMap { eventA =>
+        otherEvents.flatMap { eventB =>
+          val commonAttributes = eventA.attributes.filter { attrA =>
+            eventB.attributes.exists(attrB =>
+              attrB.name == attrA.name && attrB.value == attrA.value
+            )
+          }
 
-  /** "Drill across" operation used to combine events from different cubes that
-    * share common attribute names and values.
-    *
-    * @param events
-    *   first set of events
-    * @param otherEvents
-    *   second set of events to combine with
-    * @param createEvent
-    *   function to construct a new event from shared attributes and combined
-    *   measures
-    * @tparam A1
-    *   the type of attributes in the first set of events
-    * @tparam M1
-    *   the type of measures in the first set of events
-    * @tparam A2
-    *   the type of attributes in the second set of events
-    * @tparam M2
-    *   the type of measures in the second set of events
-    * @return
-    *   set of events resulting from the combination of matching events from
-    *   both sets
-    */
-  def drillAcross[
-      A1 <: Attribute,
-      M1 <: Measure[_],
-      A2 <: Attribute,
-      M2 <: Measure[_]
-  ](
-      events: Iterable[Event[A1, M1]],
-      otherEvents: Iterable[Event[A2, M2]],
-      createEvent: EventConstructor[A1, M1 | M2]
-  ): Iterable[Event[A1, M1 | M2]] = {
-    events.flatMap { eventA =>
-      otherEvents.flatMap { eventB =>
-        val commonAttributes = eventA.attributes.filter { attrA =>
-          eventB.attributes.exists(attrB =>
-            attrB.name == attrA.name && attrB.value == attrA.value
-          )
-        }
-
-        if (commonAttributes.nonEmpty) {
-          val combinedMeasures: Iterable[M1 | M2] =
-            eventA.measures ++ eventB.measures
-          Some(createEvent(commonAttributes, combinedMeasures))
-        } else {
-          None
+          if (commonAttributes.nonEmpty) {
+            val combinedMeasures = eventA.measures ++ eventB.measures
+            Some(createEvent(commonAttributes, combinedMeasures))
+          } else {
+            None
+          }
         }
       }
     }
-  }
+
+
