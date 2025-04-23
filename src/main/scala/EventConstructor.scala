4ee997ed8fe45c6f4f3c1de21eafabacaa9213a2
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-04-23 16:29:56 +0200

    fix checks

--- src/main/scala/EventConstructor.scala
+++ src/main/scala/EventConstructor.scala
@@ -1,5 +1,9 @@
-/** A type alias for a function to construct an Event given its dimensions and
+import MultidimensionalModel.Attribute
+import MultidimensionalModel.Event
+import MultidimensionalModel.Measure
+
+/** A type alias for a function to construct an Event given its attributes and
   * measures
   */
-type EventConstructor[A <: EventAttribute, M <: EventMeasure[_]] =
+type EventConstructor[A <: Attribute, M <: Measure[_]] =
   (Iterable[A], Iterable[M]) => Event[A, M]
