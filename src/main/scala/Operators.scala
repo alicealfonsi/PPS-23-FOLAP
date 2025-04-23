5b8526f86884ad211a601e8c6d17c78546c8141f
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-04-23 15:52:48 +0200

    add doc

--- src/main/scala/Operators.scala
+++ src/main/scala/Operators.scala
@@ -1,5 +1,24 @@
 import MultidimensionalModel._
+
+/** Operators for querying and manipulating events of a multidimensional data
+  * warehouse
+  */
 object Operators:
+
+  /** "Slice and dice" operation used to filter events by matching attribute
+    * names and values.
+    *
+    * @param events
+    *   events to filter
+    * @param filters
+    *   attributes to match by name and value
+    * @tparam A
+    *   type of attributes
+    * @tparam M
+    *   type of measures
+    * @return
+    *   events that match all filters
+    */
 
   def sliceAndDice[A <: Attribute, M <: Measure[_]](
       events: Iterable[Event[A, M]],
