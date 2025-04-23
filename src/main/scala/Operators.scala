9a997f994312e38c0eee7f6bb60330e89666606f
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-04-23 10:14:22 +0200

    fix format

--- src/main/scala/Operators.scala
+++ src/main/scala/Operators.scala
@@ -1,28 +1,26 @@
 import MultidimensionalModel._
 
 object Operators:
-  
-    def drillAcross[A <: Attribute, M <: Measure[_]](
-        events: Iterable[Event[A, M]],
-        otherEvents: Iterable[Event[A, M]],
-        createEvent: EventConstructor[A, M]
-    ): Iterable[Event[A, M]] = {
-      events.flatMap { eventA =>
-        otherEvents.flatMap { eventB =>
-          val commonAttributes = eventA.attributes.filter { attrA =>
-            eventB.attributes.exists(attrB =>
-              attrB.name == attrA.name && attrB.value == attrA.value
-            )
-          }
 
-          if (commonAttributes.nonEmpty) {
-            val combinedMeasures = eventA.measures ++ eventB.measures
-            Some(createEvent(commonAttributes, combinedMeasures))
-          } else {
-            None
-          }
+  def drillAcross[A <: Attribute, M <: Measure[_]](
+      events: Iterable[Event[A, M]],
+      otherEvents: Iterable[Event[A, M]],
+      createEvent: EventConstructor[A, M]
+  ): Iterable[Event[A, M]] = {
+    events.flatMap { eventA =>
+      otherEvents.flatMap { eventB =>
+        val commonAttributes = eventA.attributes.filter { attrA =>
+          eventB.attributes.exists(attrB =>
+            attrB.name == attrA.name && attrB.value == attrA.value
+          )
+        }
+
+        if (commonAttributes.nonEmpty) {
+          val combinedMeasures = eventA.measures ++ eventB.measures
+          Some(createEvent(commonAttributes, combinedMeasures))
+        } else {
+          None
         }
       }
     }
-
-
+  }
