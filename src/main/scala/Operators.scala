1c43428ff99cd32e5f88375d89103385bdec5e68
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-04-23 16:02:46 +0200

    add doc

--- src/main/scala/Operators.scala
+++ src/main/scala/Operators.scala
@@ -1,7 +1,27 @@
 import MultidimensionalModel._
 
+/** Operators for querying and manipulating events of a multidimensional data
+  * warehouse
+  */
 object Operators:
 
+  /** "Drill across" operation used to combine events from different cubes that
+    * share common attribute names and values.
+    *
+    * @param events
+    *   first set of events
+    * @param otherEvents
+    *   second set of events to combine with
+    * @param createEvent
+    *   function to construct a new event from shared attributes and combined
+    *   measures
+    * @tparam A
+    *   type of attributes
+    * @tparam M
+    *   type of measures
+    * @return
+    *   events resulting from the combination of matching events from both sets
+    */
   def drillAcross[A <: Attribute, M <: Measure[_]](
       events: Iterable[Event[A, M]],
       otherEvents: Iterable[Event[A, M]],
