fc0ea8d80d9dfebaeb14db3881f73f81d0952bdc
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-04-23 16:07:23 +0200

    rm eventConstructor

--- src/main/scala/EventConstructor.scala
+++
@@ -1,6 +0,0 @@
-import MultidimensionalModel.Attribute
-import MultidimensionalModel.Event
-import MultidimensionalModel.Measure
-
-type EventConstructor[A <: Attribute, M <: Measure[_]] =
-  (Iterable[A], Iterable[M]) => Event[A, M]
