28d9643a959de92418f73234858bfa4ef022fe19
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-04-25 12:32:38 +0200

    Fix rollUp signature

--- src/main/scala/Operators.scala
+++ src/main/scala/Operators.scala
@@ -1,7 +1,7 @@
 import MultidimensionalModel._
 
 object Operators:
-  def rollUp[A <: EventAttribute, M <: Measure[_]](
+  def rollUp[A <: EventAttribute, M <: EventMeasure[_]](
       events: Iterable[Event[A, M]]
   )(groupBy: String): Iterable[Event[A, M]] =
     if events.forall(e =>
