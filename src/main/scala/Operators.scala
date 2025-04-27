b9be5a96f447fa2e49ee7647411a85f7e903e4cf
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-04-27 09:15:08 +0200

    Refactor rollUp by adding matchAttribute method

--- src/main/scala/Operators.scala
+++ src/main/scala/Operators.scala
@@ -4,7 +4,15 @@ object Operators:
   def rollUp[A <: EventAttribute, M <: EventMeasure[_]](
       events: Iterable[Event[A, M]]
   )(groupBy: String): Iterable[Event[A, M]] =
-    if events.forall(e =>
+    if events.matchAttribute(groupBy)
+    then List()
+    else events
+
+  extension [A <: EventAttribute, M <: EventMeasure[_]](
+      events: Iterable[Event[A, M]]
+  )
+    private def matchAttribute(groupBy: String): Boolean =
+      events.forall(e =>
         e.dimensions.exists(
           _.name == groupBy
         ) | e.dimensions.forall(d =>
@@ -13,5 +21,3 @@ object Operators:
           )
         )
       )
-    then List()
-    else events
