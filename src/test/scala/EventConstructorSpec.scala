20ad681a41d411525c6ba0fe7d4dca9239cffc59
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-04-27 19:39:58 +0200

    Fix EventConstructorSpec

--- src/test/scala/EventConstructorSpec.scala
+++ src/test/scala/EventConstructorSpec.scala
@@ -1,21 +1,27 @@
-import MultidimensionalModel.Event
 import org.scalatest._
 
 import flatspec._
 import matchers._
 
-private case class ResultEvent(
-    override val dimensions: Iterable[ExampleAttribute],
-    override val measures: Iterable[ExampleMeasure[_]]
-) extends Event[ExampleAttribute, ExampleMeasure[_]]
-
 class EventConstructorSpec extends AnyFlatSpec with should.Matchers:
-  val dimensions: Iterable[ExampleAttribute] = List(ExampleAttribute())
-  val measures: Iterable[ExampleMeasure[_]] = List(ExampleMeasure(10))
-  def createEvent: EventConstructor[ExampleAttribute, ExampleMeasure[_]] =
+
+  private case class ResultEvent(
+      override val dimensions: Iterable[ExampleEventAttribute],
+      override val measures: Iterable[ExampleEventMeasure[_]]
+  ) extends Event[ExampleEventAttribute, ExampleEventMeasure[_]]
+
+  val dimensions: Iterable[ExampleEventAttribute] = List(
+    DimensionExampleAttribute(None, "")
+  )
+  val measures: Iterable[ExampleEventMeasure[_]] = List(
+    QuantityExampleMeasure(10)
+  )
+
+  def createEvent
+      : EventConstructor[ExampleEventAttribute, ExampleEventMeasure[_]] =
     (
-        dimensions: Iterable[ExampleAttribute],
-        measures: Iterable[ExampleMeasure[_]]
+        dimensions: Iterable[ExampleEventAttribute],
+        measures: Iterable[ExampleEventMeasure[_]]
     ) => ResultEvent(dimensions, measures)
 
   "An EventConstructor" should "create an Event with given dimensions and measures" in:
