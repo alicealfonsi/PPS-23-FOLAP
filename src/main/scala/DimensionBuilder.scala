75ad6be626ba01a00f81d5e8313392a043cef141
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-05-08 10:51:51 +0200

    Implemented DimensionBuilder

---
+++ src/main/scala/DimensionBuilder.scala
@@ -0,0 +1,5 @@
+case class Dimension(name: String, attributes: Seq[String])
+
+object DimensionBuilder:
+  extension (name: String)
+    def dimension(attributes: Seq[String]): Dimension = Dimension(name, attributes)
---
+++ src/test/scala/DimensionBuilderSpec.scala
@@ -0,0 +1,11 @@
+import SeqBuilder.-->
+import DimensionBuilder.dimension
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class DimensionBuilderSpec extends AnyFlatSpec with should.Matchers:
+  "A DimensionBuilder" should "accept a dimension as a sequence of strings" in:
+    val attributes = "town" --> "province" --> "region" --> "country"
+    val geoMeasure = "geo" dimension attributes
+    geoMeasure.name shouldEqual "geo"
+    geoMeasure.attributes shouldEqual attributes
