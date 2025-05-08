53b905aeda575f11244ddd1e733fae4900dd774c
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-05-08 10:38:34 +0200

    SeqBuilder can be used on scalars and vectors alike

--- src/main/scala/SeqBuilder.scala
+++ src/main/scala/SeqBuilder.scala
@@ -1,4 +1,13 @@
 object SeqBuilder:
-  extension (head: String)
+  trait ToSeq[T]:
+    def toSeq: Seq[T]
+
+  implicit class SeqableString(val s: String) extends ToSeq[String]:
+    override def toSeq: Seq[String] = Seq(s)
+
+  implicit class SeqableStringSeq(val s: Seq[String]) extends ToSeq[String]:
+    override def toSeq: Seq[String] = s
+
+  extension (head: ToSeq[String])
     infix def -->(tail: String): Seq[String] =
-      Seq(head, tail)
+      head.toSeq.appended(tail)
--- src/test/scala/SeqBuilderSpec.scala
+++ src/test/scala/SeqBuilderSpec.scala
@@ -3,7 +3,6 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 import scala.language.postfixOps
-import scala.math.Numeric.Implicits.infixNumericOps
 
 class SeqBuilderSpec extends AnyFlatSpec with should.Matchers:
   "A SeqBuilder" should "create a Seq from two distinct strings" in:
@@ -11,3 +10,10 @@ class SeqBuilderSpec extends AnyFlatSpec with should.Matchers:
     val second = "second"
     val seq = first --> second
     seq shouldEqual Seq(first, second)
+
+  "A SeqBuilder" should "create a Seq from a list and a string" in :
+    val first = "first"
+    val second = "second"
+    val third = "third"
+    val seq = first --> second --> third
+    seq shouldEqual Seq(first, second, third)
