a00453f785586967c613e7ba1bc65d04f383603c
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-05-12 18:27:54 +0200

    Reformat doc

--- src/main/scala/DSLUtils.scala
+++ src/main/scala/DSLUtils.scala
@@ -1,11 +1,11 @@
-/**
- * This object contains utility functions for the DSL
- */
+/** This object contains utility functions for the DSL
+  */
 object DSLUtils:
-  /**
-   * Transform a string into a valid class name
-   * @param input the untrusted input
-   * @return the PascalCase version of that string
-   */
+  /** Transform a string into a valid class name
+    * @param input
+    *   the untrusted input
+    * @return
+    *   the PascalCase version of that string
+    */
   def sanitise(input: String): String =
     input.split(" ").map(_.capitalize).foldLeft("")(_ + _)
