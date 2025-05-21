f3ca6339217bea3728d40c047cc21ac501d80772
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-05-21 20:51:04 +0200

    EventBuilder: it should build an Event from a measure with the word having

--- typingDSL/src/main/scala/folap/typingDSL/EventBuilder.scala
+++ typingDSL/src/main/scala/folap/typingDSL/EventBuilder.scala
@@ -7,6 +7,8 @@ case class Event(
 ):
   def having(dimension: Dimension): Event =
     Event(name, dimensions :+ dimension, measures)
+  def having(measure: Measure[_]): Event =
+    Event(name, dimensions, measures :+ measure)
   def and(dimension: Dimension): Event = having(dimension)
 
 object EventBuilder:
--- typingDSL/src/test/scala/folap/typingDSL/EventBuilderSpec.scala
+++ typingDSL/src/test/scala/folap/typingDSL/EventBuilderSpec.scala
@@ -5,11 +5,13 @@ import org.scalatest.matchers.should
 
 import EventBuilder.event
 import DimensionBuilder.dimension
+import MeasureDSL.measure
 
 class EventBuilderSpec extends AnyFlatSpec with should.Matchers:
   val eventName: String = "TestEvent"
   val d1: Dimension = "dimension1" dimension Seq("")
   val d2: Dimension = "dimension2" dimension Seq("")
+  val m1: Measure[_] = "" measure "measure1" as "Int"
 
   "An EventBuilder" should "build an Event from a name with the word named" in:
     val e = event named eventName
@@ -22,3 +24,7 @@ class EventBuilderSpec extends AnyFlatSpec with should.Matchers:
   it should "build an Event from more than one dimension with the word having followed by the word and" in:
     val e = event named eventName having d1 and d2
     e shouldEqual Event(eventName, Seq(d1, d2), Seq())
+
+  it should "build an Event from a measure with the word having" in:
+    val e = event named eventName having m1
+    e shouldEqual Event(eventName, Seq(), Seq(m1))
