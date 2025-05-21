24b1d44f3f8fa3f86805ae120c2f9fa358a7db3f
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-05-21 22:28:09 +0200

    Refactor and methods using union type

--- typingDSL/src/main/scala/folap/typingDSL/EventBuilder.scala
+++ typingDSL/src/main/scala/folap/typingDSL/EventBuilder.scala
@@ -8,8 +8,7 @@ case class Event(
   def having(property: Dimension | Measure[_]): Event = property match
     case d: Dimension => Event(name, dimensions :+ d, measures)
     case m: Measure[_] => Event(name, dimensions, measures :+ m)
-  def and(dimension: Dimension): Event = having(dimension)
-  def and(measure: Measure[_]): Event = having(measure)
+  def and(property: Dimension | Measure[_]): Event = having(property)
 
 object EventBuilder:
   case class EventWord():
