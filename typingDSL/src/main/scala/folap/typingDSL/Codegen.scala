34471692aa208ba059fdb93d61cb8bd64e6d45f4
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-05-24 21:56:07 +0200

    Refactor ugly code

--- typingDSL/src/main/scala/folap/typingDSL/Codegen.scala
+++ typingDSL/src/main/scala/folap/typingDSL/Codegen.scala
@@ -6,13 +6,17 @@ import folap.typingDSL.DSLUtils.indent
 object Codegen:
   def generate(dimension: Dimension): String =
     val sanitised = sanitise(dimension.name)
-    s"sealed trait ${sanitised}Dimension\nobject ${sanitised}Dimension:" +
-      dimension.attributes
-        .zip(dimension.attributes.tail)
-        .map((current, parent) => (sanitise(current), sanitise(parent)))
-        .map((current, parent) =>
-          s"case class ${current}(value: String, parent: ${parent})"
-        )
-        .map(indent(_, 2))
-        .mkString("\n") +
-      s"case class ${{ sanitise(dimension.attributes.last) }}(value: String)"
+    val traitAndObjectHead =
+      s"sealed trait ${sanitised}Dimension\nobject ${sanitised}Dimension:"
+    val objectBody = dimension.attributes
+      .zip(dimension.attributes.tail)
+      .map((current, parent) => (sanitise(current), sanitise(parent)))
+      .map((current, parent) =>
+        s"case class ${current}(value: String, parent: ${parent})"
+      )
+      .map(indent(_, 2))
+      .mkString("\n")
+    val lastLevelName = sanitise(dimension.attributes.last)
+    val lastLevel = s"case class ${lastLevelName}(value: String)"
+
+    traitAndObjectHead + objectBody + lastLevel
