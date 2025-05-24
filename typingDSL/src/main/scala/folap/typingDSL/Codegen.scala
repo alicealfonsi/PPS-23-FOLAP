b242edc1ebcb26abeb9c4a24dcb331df58efbfe9
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-05-24 21:54:19 +0200

    Generate dimensions, missing trait implementation

--- typingDSL/src/main/scala/folap/typingDSL/Codegen.scala
+++ typingDSL/src/main/scala/folap/typingDSL/Codegen.scala
@@ -1,9 +1,18 @@
 package folap.typingDSL
 
 import folap.typingDSL.DSLUtils.sanitise
+import folap.typingDSL.DSLUtils.indent
 
 object Codegen:
   def generate(dimension: Dimension): String =
     val sanitised = sanitise(dimension.name)
-    s"""sealed trait ${sanitised}Dimension
-object ${sanitised}Dimension:"""
+    s"sealed trait ${sanitised}Dimension\nobject ${sanitised}Dimension:" +
+      dimension.attributes
+        .zip(dimension.attributes.tail)
+        .map((current, parent) => (sanitise(current), sanitise(parent)))
+        .map((current, parent) =>
+          s"case class ${current}(value: String, parent: ${parent})"
+        )
+        .map(indent(_, 2))
+        .mkString("\n") +
+      s"case class ${{ sanitise(dimension.attributes.last) }}(value: String)"
--- typingDSL/src/test/scala/folap/typingDSL/CodegenSpec.scala
+++ typingDSL/src/test/scala/folap/typingDSL/CodegenSpec.scala
@@ -15,3 +15,13 @@ class CodegenSpec extends AnyFlatSpec with should.Matchers:
     generate(geoMeasure) should startWith(
       "sealed trait GeoDimension\nobject GeoDimension:"
     )
+
+  "Code generation" should "generate a case class for each attribute" in:
+    val attributes = "town" --> "province" --> "region" --> "country"
+    val geoMeasure = "geo" dimension attributes
+    generate(geoMeasure) should endWith(
+      """  case class Town(value: String, parent: Province)
+  case class Province(value: String, parent: Region)
+  case class Region(value: String, parent: Contry)
+  case class Country(value: String)"""
+    )
