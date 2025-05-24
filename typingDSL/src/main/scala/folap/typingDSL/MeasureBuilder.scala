1015f43f69009212f1846cd7a395c28e4dd3d43f
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-05-24 22:13:35 +0200

    Leaner measure DSL

--- typingDSL/src/main/scala/folap/typingDSL/MeasureBuilder.scala
+++ typingDSL/src/main/scala/folap/typingDSL/MeasureBuilder.scala
@@ -2,11 +2,5 @@ package folap.typingDSL
 
 object MeasureDSL:
 
-  class MeasureName(val name: String)
-
-  extension (space: String)
-    infix def measure(name: String): MeasureName = MeasureName(name)
-
-  extension (m: MeasureName)
-    infix def as(typology: MeasureType): Measure =
-      Measure(m.name, typology)
+  extension (t: MeasureType)
+    infix def measure(name: String): Measure = Measure(name, t)
--- typingDSL/src/test/scala/folap/typingDSL/MeasureBuilderSpec.scala
+++ typingDSL/src/test/scala/folap/typingDSL/MeasureBuilderSpec.scala
@@ -3,25 +3,24 @@ package folap.typingDSL
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import MeasureDSL.as
 import MeasureDSL.measure
 
 class MeasureBuilderSpec extends AnyFlatSpec with Matchers:
 
   "The Measure DSL" should "construct a Measure[Int] from DSL" in:
-    val m = "" measure "price" as Int
+    val m = Int measure "price"
     m shouldBe Measure("price", Int)
 
   it should "construct a Measure[Double] from DSL" in:
-    val m = "" measure "price" as Double
+    val m = Double measure "price"
     m shouldBe Measure("price", Double)
 
   it should "construct a Measure[Long] from DSL" in:
-    val m = "" measure "price" as Long
+    val m = Long measure "price"
     m.name shouldBe "price"
     m shouldBe Measure("price", Long)
 
   it should "construct a Measure[Float] from DSL" in:
-    val m = "" measure "price" as Float
+    val m = Float measure "price"
     m.name shouldBe "price"
     m shouldBe Measure("price", Float)
