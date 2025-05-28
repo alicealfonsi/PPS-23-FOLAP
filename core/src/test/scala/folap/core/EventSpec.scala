8d37999ad0c91b5e98a654f19add0d912e0f47ef
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-05-28 10:55:47 +0200

    Fix Measure in EventSpec by removing the type parameter and setting the abstract type T to the concrete type Int for QuantityExampleMeasure

--- core/src/test/scala/folap/core/EventSpec.scala
+++ core/src/test/scala/folap/core/EventSpec.scala
@@ -7,7 +7,7 @@ import flatspec._
 import matchers._
 
 trait ExampleEventAttribute extends Attribute
-trait ExampleEventMeasure[T] extends Measure[T]
+trait ExampleEventMeasure extends Measure
 private case class DimensionExampleAttribute(
     override val parent: Option[TopAttribute],
     override val value: String
@@ -16,18 +16,24 @@ private case class QuantityExampleMeasure(value: Int)
     extends ExampleEventMeasure[Int]:
   override def fromRaw(value: Int): Measure[Int] = QuantityExampleMeasure(value)
 private val value: Int = 10
+private case class QuantityExampleMeasure(override val value: Int)
+    extends ExampleEventMeasure:
+  type T = Int
+  override def fromRaw(value: Int): QuantityExampleMeasure =
+    QuantityExampleMeasure(value)
 private case class ExampleEvent()
-    extends Event[ExampleEventAttribute, ExampleEventMeasure[_]]:
+    extends Event[ExampleEventAttribute, ExampleEventMeasure]:
   override def dimensions: Iterable[ExampleEventAttribute] =
     List(DimensionExampleAttribute(Some(TopAttribute()), ""))
   override def measures: Iterable[ExampleEventMeasure[_]] = List(
     QuantityExampleMeasure(value)
+  override def measures: Iterable[ExampleEventMeasure] = List(
   )
 
 class EventSpec extends AnyFlatSpec with should.Matchers:
   val dim: DimensionExampleAttribute =
     DimensionExampleAttribute(Some(TopAttribute()), "")
-  val event: Event[ExampleEventAttribute, ExampleEventMeasure[_]] =
+  val event: Event[ExampleEventAttribute, ExampleEventMeasure] =
     ExampleEvent()
 
   "An Event" should "have a list of dimensions" in:
