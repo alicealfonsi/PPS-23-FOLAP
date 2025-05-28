6f659035671c9bdb61fb9168445348ebba27f3d0
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-05-28 11:02:39 +0200

    Add RevenueExampleMeasure and update EventSpec: an Event should have a list of measures which can be of different types

--- core/src/test/scala/folap/core/EventSpec.scala
+++ core/src/test/scala/folap/core/EventSpec.scala
@@ -12,22 +12,25 @@ private case class DimensionExampleAttribute(
     override val parent: Option[TopAttribute],
     override val value: String
 ) extends ExampleEventAttribute
-private case class QuantityExampleMeasure(value: Int)
-    extends ExampleEventMeasure[Int]:
-  override def fromRaw(value: Int): Measure[Int] = QuantityExampleMeasure(value)
-private val value: Int = 10
 private case class QuantityExampleMeasure(override val value: Int)
     extends ExampleEventMeasure:
   type T = Int
   override def fromRaw(value: Int): QuantityExampleMeasure =
     QuantityExampleMeasure(value)
+private case class RevenueExampleMeasure(override val value: Double)
+    extends ExampleEventMeasure:
+  type T = Double
+  override def fromRaw(value: Double): RevenueExampleMeasure =
+    RevenueExampleMeasure(value)
+private val valueQ: Int = 10
+private val valueR: Double = 17.5
 private case class ExampleEvent()
     extends Event[ExampleEventAttribute, ExampleEventMeasure]:
   override def dimensions: Iterable[ExampleEventAttribute] =
     List(DimensionExampleAttribute(Some(TopAttribute()), ""))
-  override def measures: Iterable[ExampleEventMeasure[_]] = List(
-    QuantityExampleMeasure(value)
   override def measures: Iterable[ExampleEventMeasure] = List(
+    QuantityExampleMeasure(valueQ),
+    RevenueExampleMeasure(valueR)
   )
 
 class EventSpec extends AnyFlatSpec with should.Matchers:
@@ -42,5 +45,8 @@ class EventSpec extends AnyFlatSpec with should.Matchers:
   it should "have a list of attributes" in:
     event.attributes shouldEqual List(dim, TopAttribute())
 
-  it should "have a list of measures" in:
-    event.measures shouldEqual List(QuantityExampleMeasure(10))
+  it should "have a list of measures which can be of different types" in:
+    event.measures shouldEqual List(
+      QuantityExampleMeasure(10),
+      RevenueExampleMeasure(17.5)
+    )
