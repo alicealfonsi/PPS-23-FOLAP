b41a9a03caf3e073f6d507de0ce067040738b17b
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-05-30 14:24:05 +0200

    Modify aggregateBySum method to accept a group-by set and use the aggregate method

--- core/src/main/scala/folap/core/Operational.scala
+++ core/src/main/scala/folap/core/Operational.scala
@@ -1,7 +1,5 @@
 package folap.core
 
-trait Operational[E]:
-  extension (e: E) def sum(other: E)(groupByAttribute: String): E
 import MultidimensionalModel._
 
 trait Operational[A <: Attribute, M <: Measure, E <: Event[A, M]]:
@@ -9,7 +7,7 @@ trait Operational[A <: Attribute, M <: Measure, E <: Event[A, M]]:
     def sum(other: E)(groupBySet: Iterable[String]): E
     def aggregate(groupBySet: Iterable[String]): E
   extension (events: Iterable[E])
-    def aggregateBySum(groupByAttribute: String): E =
-      events.tail.foldLeft(events.head)((acc, el) =>
-        acc.sum(el)(groupByAttribute)
-      )
+    def aggregateBySum(groupBySet: Iterable[String]): E =
+      if events.size == 1 then events.head.aggregate(groupBySet)
+      else
+        events.tail.foldLeft(events.head)((acc, el) => acc.sum(el)(groupBySet))
--- core/src/test/scala/folap/core/OperationalSpec.scala
+++ core/src/test/scala/folap/core/OperationalSpec.scala
@@ -17,7 +17,6 @@ class OperationalSpec extends AnyFlatSpec with should.Matchers:
       QuantitySold(3)
     )
 
-  "Operational[SalesEvent]" should "return the correct aggregation by sum of SalesEvent" in:
   "Operational[SalesEvent]" should "return the correct aggregation of a SalesEvent based on the specified group-by set" in:
     event3.aggregate(List("Type", "Nation")) shouldEqual
       SalesEvent(
@@ -26,9 +25,11 @@ class OperationalSpec extends AnyFlatSpec with should.Matchers:
         QuantitySold(7)
       )
 
+  "Operational[SalesEvent]" should "return the correct aggregation by sum of SalesEvents based on the specified group-by set" in:
     List(event1, event2, event3).aggregateBySum(
-      "Nation"
+      List("Nation", "Category")
     ) shouldEqual SalesEvent(
       Nation(Some(TopAttribute()), "Italy"),
+      Category(Some(ProductAttribute.TopAttribute()), "Groceries"),
       QuantitySold(10)
     )
