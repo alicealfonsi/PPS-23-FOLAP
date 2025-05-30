94e1497e9602ec6449cabdc32f5528fde2cc8bc6
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-05-30 15:46:41 +0200

    moved visualize method  to another package

--- core/src/test/scala/folap/olapDSL/SalesCube.scala
+++ core/src/test/scala/folap/olapDSL/SalesCube.scala
@@ -7,8 +7,9 @@ import folap.core.MultidimensionalModel._
 import folap.olapDSL.AttributeDSLBuilder._
 import folap.olapDSL.AttributeSeqBuilder._
 import folap.olapDSL.QueryDSLBuilder._
+import folap.utils.visualize
 
-import scala.reflect.Selectable.reflectiveSelectable
+
 
 trait Dimension extends Attribute
 trait DateDimension extends Dimension
@@ -195,45 +196,12 @@ given EventConstructor[A <: Attribute, M <: Measure]: EventConstructor[A, M] =
 val careEvents = Iterable(careEvent1, careEvent2)
 val CustomerCare = QueryDSL(careEvents)
 
-def extractAllDimensions(attr: Attribute): List[Attribute] =
-  attr match
-    case TopAttribute() => Nil
-    case a =>
-      a.parent.toList.flatMap(extractAllDimensions) ++ List(a)
 
-def visualizeCube(events: Iterable[Event[_, _]]): Unit =
-  println("===== Cube Result =====")
-  events.zipWithIndex.foreach { (event, idx) =>
-    println(s"--- Event ${idx + 1} ---")
-
-    val expanded = event.dimensions
-      .flatMap(extractAllDimensions)
-      .filter(_ != TopAttribute())
-
-    val seen = scala.collection.mutable.LinkedHashSet[String]()
-    val orderedDims = expanded.filter { a =>
-      val key = a.getClass.getName + ":" + a.value
-      if seen.contains(key) then false else { seen += key; true }
-    }
-
-    orderedDims.foreach { attr =>
-      val name = attr.getClass.getSimpleName.replace("Attribute", "")
-      println(s"$name: ${attr.value}")
-    }
-
-    event.measures.foreach { m =>
-      val name = m.getClass.getSimpleName
-      val value = m.asInstanceOf[{ def value: Any }].value
-      println(s"$name: $value")
-    }
-
-    println()
-  }
 
 @main def main(): Unit =
-  Sales where ("Product" is "iPhone 14" and ("City" is "Berlin"))
-
+  val filtered = Sales where ("Product" is "iPhone 14" and ("City" is "Berlin"))
+  visualize(filtered.cube)
   // visualizeCube(filtered.cube)
   val union = Sales union CustomerCare
-  visualizeCube(union.cube)
-  Max of Sales
+  visualize(union.cube)
+  val aggregation = Max of Sales
