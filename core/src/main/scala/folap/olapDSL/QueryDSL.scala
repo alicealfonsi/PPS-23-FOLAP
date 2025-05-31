df4fef9f84815ba2bfd8634dd04e1a14a84ba5ad
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-05-31 16:13:44 +0200

    change doc

--- core/src/main/scala/folap/olapDSL/QueryDSL.scala
+++ core/src/main/scala/folap/olapDSL/QueryDSL.scala
@@ -5,9 +5,9 @@ import folap.core._
 /** Represents a collection of OLAP events (cube)
   *
   * @tparam A
-  *   type of attribute used in the events, which must extend EventAttribute.
+  *   type of attribute used in the events, which must extend Attribute.
   * @tparam M
-  *   type of measure used in the events, which must extend EventMeasure[_].
+  *   type of measure used in the events, which must extend Measure.
   * @param cube
   *   a collection of events
   *
