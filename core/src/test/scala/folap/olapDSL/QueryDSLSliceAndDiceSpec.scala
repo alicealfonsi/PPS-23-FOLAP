3ea3841c8d95285f5769e14c3015309e9e1dcd21
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2025-05-31 12:06:09 +0200

    Fix Idea warnings

--- core/src/test/scala/folap/olapDSL/QueryDSLSliceAndDiceSpec.scala
+++ core/src/test/scala/folap/olapDSL/QueryDSLSliceAndDiceSpec.scala
@@ -20,35 +20,34 @@ class QueryDSLSliceAndDiceSpec extends AnyFlatSpec with Matchers:
       override val value: String,
       override val parent: Option[TopAttribute]
   ) extends SalesAttribute
-  case class TotSalesMeasure(val value: Int) extends SalesMeasure:
+  case class TotSalesMeasure(override val value: Int) extends SalesMeasure:
     type T = Int
-    override def fromRaw(value: Int): TotSalesMeasure = TotSalesMeasure(value)
 
   case class SalesEvent(
       override val dimensions: Iterable[SalesAttribute],
       override val measures: Iterable[SalesMeasure]
   ) extends Event[SalesAttribute, SalesMeasure]
-  val event1 = SalesEvent(
+  val event1: SalesEvent = SalesEvent(
     dimensions =
       Seq(NationAttribute("Italy", None), YearAttribute("2024", None)),
     measures = Seq(TotSalesMeasure(100))
   )
 
-  val event2 = SalesEvent(
+  val event2: SalesEvent = SalesEvent(
     dimensions =
       Seq(NationAttribute("France", None), YearAttribute("2024", None)),
     measures = Seq(TotSalesMeasure(150))
   )
 
-  val event3 = SalesEvent(
+  val event3: SalesEvent = SalesEvent(
     dimensions =
       Seq(NationAttribute("Italy", None), YearAttribute("2023", None)),
     measures = Seq(TotSalesMeasure(120))
   )
 
-  val events = Seq(event1, event2, event3)
+  val events: Seq[SalesEvent] = Seq(event1, event2, event3)
 
-  val Sales = QueryDSL(events)
+  val Sales: QueryDSL[SalesAttribute, SalesMeasure] = QueryDSL(events)
 
   "sliceAndDice" should "filter events by a single attribute (slice)" in:
     val Nation = "Nation"
