9cb5ec654987a4aaa01c2f62ab3251b5e9652007
Claudia Giannelli Taccarino <claudia.giannelli2@studio.unibo.it>
2025-06-04 10:51:04 +0200

    fix format

--- examples/src/main/scala/mycompany/dw/GenerateSalesCube.scala
+++ examples/src/main/scala/mycompany/dw/GenerateSalesCube.scala
@@ -7,10 +7,11 @@ import folap.modelDefinition.MeasureDSL.measure
 import folap.modelDefinition.SeqBuilder.-->
 
 @main def generate: Unit =
-    val temporal = "temporal" dimension "Month" --> "Quarter" --> "Year"
-    val geographic = "geographic" dimension "City" --> "Country" --> "Region"
-    val product = "product" dimension "Product" --> "SubCategory" --> "Category"
-    val revenue = measure named "Revenue" as Double
-    val quantity = measure named "Quantity" as Int
-    val sales = event named "Sales" having temporal and geographic and product and revenue and quantity
-    println(Codegen.generate(sales))
+  val temporal = "temporal" dimension "Month" --> "Quarter" --> "Year"
+  val geographic = "geographic" dimension "City" --> "Country" --> "Region"
+  val product = "product" dimension "Product" --> "SubCategory" --> "Category"
+  val revenue = measure named "Revenue" as Double
+  val quantity = measure named "Quantity" as Int
+  val sales =
+    event named "Sales" having temporal and geographic and product and revenue and quantity
+  println(Codegen.generate(sales))
